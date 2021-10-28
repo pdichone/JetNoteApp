394a8c651e62908d421d5cb33104cf9c63626137
pdichone <pdichone@gmail.com>
2021-10-28 15:38:46 -0700

    :star: first commit :star2:

---
+++ app/src/main/java/com/bawp/jetnote/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.bawp.jetnote
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.bawp.jetnote.ui.theme.JetNoteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetNoteTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetNoteTheme {
+        Greeting("Android")
+    }
+}
